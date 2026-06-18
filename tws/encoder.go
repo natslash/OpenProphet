bb2f0c779a3ea43a149d95415c5e1b60f18aa7c4
Shashidhara Subramanyam <natslash@gmail.com>
2026-06-18 17:30:46 +0200

    Fix PlaceOrder payload length: properly pad to EXACTLY 110 fields matching python headless generator

--- tws/encoder.go
+++ tws/encoder.go
@@ -28,7 +28,7 @@ func (e *Encoder) ReqCurrentTime() error {
 // ReqContractDetails requests contract details.
 func (e *Encoder) ReqContractDetails(reqId int64, contract Contract) error {
 	const version = "8"
-	
+
 	strikeStr := strconv.FormatFloat(contract.Strike, 'f', -1, 64)
 	if contract.Strike == 0.0 {
 		strikeStr = "0.0"
@@ -55,7 +55,7 @@ func (e *Encoder) ReqContractDetails(reqId int64, contract Contract) error {
 		"",  // secId
 		"",  // issuerId
 	}
-	
+
 	return e.writer.SendFields(fields...)
 }
 
@@ -171,122 +171,124 @@ func formatPrice(p float64) string {
 func (e *Encoder) PlaceOrder(reqId int64, contract Contract, order Order) error {
 	// Send place order message. v145+ servers do NOT expect the version string.
 	// Since we assume TWS v187, we omit the version field entirely.
-	
+
 	strikeStr := strconv.FormatFloat(contract.Strike, 'f', -1, 64)
 	if contract.Strike == 0.0 {
 		strikeStr = "0"
 	}
 
 	fields := []string{
-		strconv.Itoa(outPlaceOrder),
-		strconv.FormatInt(reqId, 10),
-		strconv.FormatInt(contract.ConId, 10),
-		contract.Symbol,
-		string(contract.SecType),
-		contract.LastTradeDateOrContractMonth,
-		strikeStr,
-		contract.Right,
-		contract.Multiplier,
-		contract.Exchange,
-		contract.PrimaryExch,
-		contract.Currency,
-		contract.LocalSymbol,
-		contract.TradingClass,
-		"", // secIdType
-		"", // secId
-		order.Action,
-		order.TotalQuantity.String(),
-		order.OrderType,
-		formatPrice(order.LmtPrice),
-		formatPrice(order.AuxPrice),
-		order.Tif,
-		order.OcaGroup,
-		order.Account,
-		"",  // openClose
-		"0", // origin
-		"",  // orderRef
-		"1", // transmit
-		"0", // parentId
-		"0", // blockOrder
-		"0", // sweepToFill
-		"0", // displaySize
-		"0", // triggerMethod
-		"0", // outsideRth
-		"0", // hidden
-		"",  // goodAfterTime
-		"0", // sharesAllocation
-		"",  // faGroup
-		"",  // faMethod
-		"",  // faPercentage
-		"",  // faProfile
-		"",  // modelCode
-		"",  // goodTillDate
-		"",  // rule80A
-		"0", // percentOffset
-		"",  // settlingFirm
-		"-1",// shortSaleSlot
-		"0", // designatedLocation
-		"",  // exemptCode
-		"",  // auctionStrategy
-		"0", // startingPrice
-		"",  // stockRefPrice
-		"",  // delta
-		"1", // stockRangeLower
-		"1", // stockRangeUpper
-		"",  // overridePercentageConstraints
-		"0", // volatility
-		"",  // volatilityType
-		"",  // deltaNeutralOrderType
-		"",  // deltaNeutralAuxPrice
-		"",  // deltaNeutralConId
-		"",  // deltaNeutralSettlingFirm
-		"0", // continuousUpdate
-		"",  // referencePriceType
-		"",  // trailStopPrice
-		"",  // trailingPercent
-		"",  // scaleInitLevelSize
-		"0", // scaleSubsLevelSize
-		"",  // scalePriceIncrement
-		"",  // scalePriceAdjustValue
-		"",  // scalePriceAdjustInterval
-		"",  // scaleProfitOffset
-		"",  // scaleAutoReset
-		"",  // scaleInitPosition
-		"",  // scaleInitFillQty
-		"",  // scaleRandomPercent
-		"",  // scaleTable
-		"0", // activeStartTime
-		"",  // activeStopTime
-		"",  // hedgeType
-		"0", // hedgeParam
-		"0", // optOutSmartRouting
-		"",  // clearingAccount
-		"",  // clearingIntent
-		"0", // notHeld
-		"",  // deltaNeutralContract
-		"0", // algoStrategy
-		"0", // algoParamsCount
-		"0", // solicited
-		"0", // whatIf
-		"",  // orderState
-		"1.7976931348623157e+308", // randomizeSize
-		"1.7976931348623157e+308", // randomizePrice
-		"1.7976931348623157e+308", // referenceContractId
-		"1.7976931348623157e+308", // peggedChangeAmount
-		"1.7976931348623157e+308", // referenceChangeAmount
-		"0", // referenceExchangeId
-		"",  // conditions
-		"",  // adjustedOrderType
-		"",  // triggerPrice
-		"1.7976931348623157e+308", // lmtPriceOffset
-		"",  // adjustedStopPrice
-		"",  // adjustedStopLimitPrice
-		"",  // adjustedTrailingAmount
-		"",  // adjustableTrailingUnit
-		"0", // softDollarTier
-		"0", // cashQty
-		"0", // dontUseAutoPriceForHedge
-		"",  // isOmsContainer
+		strconv.Itoa(outPlaceOrder),           // 0
+		strconv.FormatInt(reqId, 10),          // 1
+		strconv.FormatInt(contract.ConId, 10), // 2
+		contract.Symbol,                       // 3
+		string(contract.SecType),              // 4
+		contract.LastTradeDateOrContractMonth, // 5
+		strikeStr,                             // 6
+		contract.Right,                        // 7
+		contract.Multiplier,                   // 8
+		contract.Exchange,                     // 9
+		contract.PrimaryExch,                  // 10
+		contract.Currency,                     // 11
+		contract.LocalSymbol,                  // 12
+		contract.TradingClass,                 // 13
+		"",                                    // secIdType // 14
+		"",                                    // secId // 15
+		order.Action,                          // 16
+		order.TotalQuantity.String(),          // 17
+		order.OrderType,                       // 18
+		formatPrice(order.LmtPrice),           // 19
+		formatPrice(order.AuxPrice),           // 20
+		order.Tif,                             // 21
+		order.OcaGroup,                        // 22
+		order.Account,                         // 23
+		"",                                    // 24
+		"0",                                   // 25
+		"",                                    // 26
+		"1",                                   // 27
+		"0",                                   // 28
+		"0",                                   // 29
+		"0",                                   // 30
+		"0",                                   // 31
+		"0",                                   // 32
+		"0",                                   // 33
+		"0",                                   // 34
+		"",                                    // 35
+		"0",                                   // 36
+		"",                                    // 37
+		"",                                    // 38
+		"",                                    // 39
+		"",                                    // 40
+		"",                                    // 41
+		"",                                    // 42
+		"",                                    // 43
+		"0",                                   // 44
+		"",                                    // 45
+		"-1",                                  // 46
+		"0",                                   // 47
+		"",                                    // 48
+		"",                                    // 49
+		"0",                                   // 50
+		"",                                    // 51
+		"",                                    // 52
+		"1",                                   // 53
+		"1",                                   // 54
+		"",                                    // 55
+		"0",                                   // 56
+		"",                                    // 57
+		"",                                    // 58
+		"",                                    // 59
+		"",                                    // 60
+		"",                                    // 61
+		"0",                                   // 62
+		"",                                    // 63
+		"",                                    // 64
+		"",                                    // 65
+		"",                                    // 66
+		"0",                                   // 67
+		"",                                    // 68
+		"",                                    // 69
+		"",                                    // 70
+		"",                                    // 71
+		"",                                    // 72
+		"",                                    // 73
+		"",                                    // 74
+		"",                                    // 75
+		"",                                    // 76
+		"",                                    // 77
+		"0",                                   // 78
+		"",                                    // 79
+		"",                                    // 80
+		"0",                                   // 81
+		"0",                                   // 82
+		"",                                    // 83
+		"",                                    // 84
+		"0",                                   // 85
+		"",                                    // 86
+		"0",                                   // 87
+		"0",                                   // 88
+		"0",                                   // 89
+		"0",                                   // 90
+		"",                                    // 91
+		"1.7976931348623157e+308",             // 92
+		"1.7976931348623157e+308",             // 93
+		"1.7976931348623157e+308",             // 94
+		"1.7976931348623157e+308",             // 95
+		"1.7976931348623157e+308",             // 96
+		"0",                                   // 97
+		"",                                    // 98
+		"",                                    // 99
+		"",                                    // 100
+		"1.7976931348623157e+308",             // 101
+		"",                                    // 102
+		"",                                    // 103
+		"",                                    // 104
+		"",                                    // 105
+		"0",                                   // 106
+		"0",                                   // 107
+		"0",                                   // 108
+		"",                                    // 109
+		"",                                    // 110
 	}
 
 	return e.writer.SendFields(fields...)
